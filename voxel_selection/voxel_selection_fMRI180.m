c04ff5692a77b12b9508b2cbc775d924988df659
Zou Shuxian <30795940+LittletreeZou@users.noreply.github.com>
2022-11-11 15:31:52 +0400

    Update voxel_selection_fMRI180.m

--- voxel_selection/voxel_selection_fMRI180.m
+++ voxel_selection/voxel_selection_fMRI180.m
@@ -1,36 +1,20 @@
+% perform voxel seletection for every subject of fMRI180 dataset
 
-
-% perform voxel seletection for every subject
-
-% array
-%subjects = ["M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M13", "M14", "M15", "M16", "M17"];
-subjects = ["P01"];
-
+subjects = ["P01", "M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M13", "M14", "M15", "M16", "M17"];
 %load('glove_v1.mat');
-%load('bert_layeravg_180.mat');
-load('bart_encoder_180_words.mat');
+load('bert_layeravg_180.mat');
 
 for i = 1:length(subjects)
     sub = char(subjects(i));
     disp(['Start computing ', sub]);
-    
     load([sub, '/data_180concepts_pictures.mat']);
     picture_fmri = examples;
-
     load([sub, '/data_180concepts_sentences.mat']);
     sent_fmri = examples;
-
     load([sub, '/data_180concepts_wordclouds.mat']);
     cloud_fmri = examples;
-
     fmri = (picture_fmri + sent_fmri + cloud_fmri) / 3;
-
     %scores = trainVoxelwiseTargetPredictionModels(fmri, glove_v1, 'meta', meta);
-    %scores = trainVoxelwiseTargetPredictionModels(fmri, bert, 'meta', meta);
-    scores = trainVoxelwiseTargetPredictionModels(fmri, bart_encoder, 'meta', meta);
-
-    save(['informative_score/bart_encoder/', sub, '_vs_scores.mat'], 'scores');
-    
-    %clearvars except glove_v1 subjects i;
-
+    scores = trainVoxelwiseTargetPredictionModels(fmri, bert, 'meta', meta);
+    save(['informative_score/bert/', sub, '_vs_scores.mat'], 'scores');
 end
