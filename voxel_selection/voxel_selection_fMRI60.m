65b9515c9e6f5ee2694952615a066d22d5fa5f8f
Zou Shuxian <30795940+LittletreeZou@users.noreply.github.com>
2022-11-11 15:42:11 +0400

    Update voxel_selection_fMRI60.m

--- voxel_selection/voxel_selection_fMRI60.m
+++ voxel_selection/voxel_selection_fMRI60.m
@@ -1,22 +1,15 @@
+% perform voxel seletection for every subject of fMRI60, the data and the code are in the same folder
 
+subjects = ['P1', 'P2', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9'];
 
-% perform voxel seletection for every subject
-
-% array
-subjects = ['s1_tianke', 's2_chenzhonghao', 's3_huangxin', 's4_luyu', 's5_liwenlu', 's6_zhangruotong',
-            's7_heqin', 's8_chenguohua', 's9_maliqun', 's10_xuweilin', 's11_zhushanshan'];
-
-load('tencent_wvs.mat');
+% word embedding for the 60 stimuli
+load('bert_layeravg.mat');
 
 for i = 1:length(subjects)
     sub = char(subjects(i));
     disp(['Start computing ', sub]);
-    
+    % load fmri data
     load([sub, '.mat']);
-
     scores = trainVoxelwiseTargetPredictionModels(fmri, wordvec);
-    
     save(['informative_score/', sub, '_vs_scores.mat'], 'scores');
-    
-
 end
